f84a99c5350ccd5e6e6f74a34b16b6e0476d5ec3
Rene Cocom <fasnimo@gmail.com>
2019-07-31 05:55:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    #can not change owners name
+    @name = name
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    #can say it's  species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    # .all returns all instances of Owner that have been created (FAILED - 6)
+    # .count returns the number of owners that have been created (FAILED - 7)
+    # .reset_all can reset the owners that have been created (FAILED - 8)
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def cats
+     Cat.all.select {|cat| cat.owner == self }
+  end
+
+  def dogs
+     Dog.all.select {|dog| dog.owner == self }
+  end
+
+  def buy_cat(name)
+   Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+   Dog.new(name, self)
+  end
+
+  def walk_dogs
+    @pets.collect do |species, instances|
+      if species == :dogs
+        instances.each do |dog|
+          dog.mood = "happy"
+        end
+      end
+    end
+  end
 end
