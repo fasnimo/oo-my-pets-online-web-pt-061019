9d3fa095ee48fd4f74b3f8df4e3210fe1c0094a0
Rene Cocom <fasnimo@gmail.com>
2019-07-31 06:24:16 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -5,7 +5,7 @@ class Cat
   def initialize(name, owner)
       @owner = owner
       @name = name
-      @owner.catss<<self
+      @owner.cats<<self
       @mood = "nervous"
       @@all << self
   end
